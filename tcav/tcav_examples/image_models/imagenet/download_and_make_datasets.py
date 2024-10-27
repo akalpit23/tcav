b643d64cef6498e2de05741a25bb1aec383431e9
Akalpit Dawkhar <130388885+akalpit23@users.noreply.github.com>
2024-10-27 18:54:29 -0400

    Update download_and_make_datasets.py

--- tcav/tcav_examples/image_models/imagenet/download_and_make_datasets.py
+++ tcav/tcav_examples/image_models/imagenet/download_and_make_datasets.py
@@ -46,8 +46,8 @@ def make_concepts_targets_and_randoms(source_dir, number_of_images_per_folder, n
         subprocess.call(['bash' , 'FetchDataAndModels.sh', source_dir])
 
     # Determine classes that we will fetch
-    imagenet_classes = ['zebra']
-    broden_concepts = ['striped', 'dotted', 'zigzagged']
+    imagenet_classes = ['eagle']
+    broden_concepts = ['feathers', 'scales', 'fur']
 
     # make targets from imagenet
     imagenet_dataframe = fetcher.make_imagenet_dataframe("./imagenet_url_map.csv")
